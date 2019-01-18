73b89a97f46db74e61794acf4d6cb6b2503eb282
mirzanove <mirzanove@gmail.com>
2019-01-18 20:52:59 +0200

    ssssssssssss

---
+++ XWalk/MainActivity.cs
@@ -0,0 +1,55 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using Android.Views;
+using Android.Content.Res;
+using System.IO;
+
+using Android.Util;
+using Android.Content;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text;
+using System.Text.RegularExpressions;
+using Org.Xwalk.Core;
+
+namespace XWalk
+{
+    [Activity(Label = "XWalk", MainLauncher = true)]
+    public class MainActivity : XWalkActivity
+    {
+
+        public static Activity fa;
+        public Org.Xwalk.Core.XWalkView xwv;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+
+        protected override void OnXWalkReady()
+        {
+            fa = this;
+
+            var view = new RelativeLayout(this.BaseContext);
+            var mp = ViewGroup.LayoutParams.MatchParent;
+            xwv = new Org.Xwalk.Core.XWalkView(this.BaseContext, this);
+            view.AddView(xwv);
+            this.AddContentView(view, new ViewGroup.LayoutParams(mp, mp));
+            xwv.SetResourceClient(new XWalkResourceClient(xwv));
+            xwv.SetUIClient(new XWalkUIClient(xwv));
+            xwv.AddJavascriptInterface(new Foo(this, xwv), "Foo");
+            xwv.LoadUrl("file:///android_asset/index.html");
+
+        }
+
+
+
+
+    }
+}
+
